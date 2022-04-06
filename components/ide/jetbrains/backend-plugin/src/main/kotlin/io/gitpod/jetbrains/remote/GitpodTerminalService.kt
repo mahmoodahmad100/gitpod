902d9bcb918cb6d5205f7c593eb3c011ed30cf5a
Victor Nogueira <felladrin@gmail.com>
2022-04-06 09:20:39 +0000

    Attach to supervisor terminals using Gitpod CLI, to avoid the issues with CloudTerminalRunner

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodTerminalService.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodTerminalService.kt
@@ -4,7 +4,6 @@
 
 package io.gitpod.jetbrains.remote
 
-import com.google.protobuf.ByteString
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.diagnostic.thisLogger
@@ -12,29 +11,19 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.openapi.wm.ex.ToolWindowManagerListener
-import com.jediterm.terminal.TtyConnector
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rdserver.terminal.BackendTerminalManager
-import com.jetbrains.rdserver.terminal.BackendTtyConnector
 import io.gitpod.supervisor.api.Status.*
 import io.gitpod.supervisor.api.StatusServiceGrpc
 import io.gitpod.supervisor.api.TerminalOuterClass
 import io.gitpod.supervisor.api.TerminalServiceGrpc
-import io.grpc.StatusRuntimeException
 import io.grpc.stub.StreamObserver
-import java.io.ByteArrayOutputStream
-import java.io.PipedInputStream
-import java.io.PipedOutputStream
 import java.util.concurrent.CompletableFuture
 import kotlinx.coroutines.*
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.guava.asDeferred
-import org.jetbrains.plugins.terminal.ShellTerminalWidget
-import org.jetbrains.plugins.terminal.TerminalTabState
 import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
 import org.jetbrains.plugins.terminal.TerminalView
-import org.jetbrains.plugins.terminal.cloud.CloudTerminalProcess
-import org.jetbrains.plugins.terminal.cloud.CloudTerminalRunner
 
 @Suppress("UnstableApiUsage", "EXPERIMENTAL_IS_NOT_ENABLED")
 @OptIn(DelicateCoroutinesApi::class)
@@ -45,7 +34,6 @@ class GitpodTerminalService(private val project: Project) : Disposable {
 
     private val lifetime = Lifetime.Eternal.createNested()
     private val terminalView = TerminalView.getInstance(project)
-    private val terminalServiceStub = TerminalServiceGrpc.newStub(GitpodManager.supervisorChannel)
     private val terminalServiceFutureStub =
             TerminalServiceGrpc.newFutureStub(GitpodManager.supervisorChannel)
     private val statusServiceStub = StatusServiceGrpc.newStub(GitpodManager.supervisorChannel)
@@ -59,8 +47,8 @@ class GitpodTerminalService(private val project: Project) : Disposable {
         GlobalScope.launch {
             getTerminalToolWindowRegisteredEvent().await()
             val tasksList = getSupervisorTasksList().await()
-            val terminalsList = getSupervisorTerminalsList().await().terminalsList
-            debug("Got a list of Supervisor terminals: ${terminalsList}")
+            val terminalsList = getSupervisorTerminalsListAsync().await().terminalsList
+            debug("Got a list of Supervisor terminals: $terminalsList")
             runInEdt {
                 for (terminalWidget in terminalView.widgets) {
                     val terminalContent =
@@ -161,38 +149,17 @@ class GitpodTerminalService(private val project: Project) : Disposable {
         return completableFuture
     }
 
-    private fun getSupervisorTerminalsList(): Deferred<TerminalOuterClass.ListTerminalsResponse> {
+    private fun getSupervisorTerminalsListAsync(): Deferred<TerminalOuterClass.ListTerminalsResponse> {
         val listTerminalsRequest = TerminalOuterClass.ListTerminalsRequest.newBuilder().build()
         return terminalServiceFutureStub.list(listTerminalsRequest).asDeferred()
     }
 
     private fun createSharedTerminal(supervisorTerminal: TerminalOuterClass.Terminal) {
         debug("Creating shared terminal '${supervisorTerminal.title}' on Backend IDE")
-        val terminalInputReader = PipedInputStream()
-        val terminalInputWriter = PipedOutputStream(terminalInputReader)
-        val terminalOutputReader = PipedInputStream()
-        val terminalOutputWriter = PipedOutputStream(terminalOutputReader)
-        val terminalProcess = CloudTerminalProcess(terminalInputWriter, terminalOutputReader)
-        val terminalRunner =
-                object : CloudTerminalRunner(project, supervisorTerminal.alias, terminalProcess) {
-                    override fun createTtyConnector(process: CloudTerminalProcess): TtyConnector {
-                        return BackendTtyConnector(project, super.createTtyConnector(process))
-                    }
-                }
 
-        val terminalRunnerId = terminalRunner.toString()
+        val terminalRunnerId = supervisorTerminal.alias
 
-        terminalView.createNewSession(
-                terminalRunner,
-                TerminalTabState().also { it.myTabName = terminalRunnerId }
-        )
-
-        val shellTerminalWidget =
-                terminalView.widgets.find { widget ->
-                    terminalView.toolWindow.contentManager.getContent(widget).tabName ==
-                            terminalRunnerId
-                } as
-                        ShellTerminalWidget
+        val shellTerminalWidget = terminalView.createLocalShellWidget(project.basePath, terminalRunnerId)
 
         backendTerminalManager.shareTerminal(shellTerminalWidget, terminalRunnerId)
 
@@ -200,177 +167,8 @@ class GitpodTerminalService(private val project: Project) : Disposable {
 
         terminalContent.putUserData(TITLE_KEY, supervisorTerminal.title)
 
-        connectSupervisorStream(
-                shellTerminalWidget,
-                supervisorTerminal,
-                terminalOutputWriter,
-                terminalInputReader
-        )
+        shellTerminalWidget.executeCommand("gp tasks attach ${supervisorTerminal.alias}")
     }
-
-    private fun connectSupervisorStream(
-            shellTerminalWidget: ShellTerminalWidget,
-            supervisorTerminal: TerminalOuterClass.Terminal,
-            terminalOutputWriter: PipedOutputStream,
-            terminalInputReader: PipedInputStream
-    ) {
-        val dataReceivedFromSupervisor = ByteArrayOutputStream()
-
-        val listenTerminalRequest =
-                TerminalOuterClass.ListenTerminalRequest.newBuilder()
-                        .setAlias(supervisorTerminal.alias)
-                        .build()
-
-        val listenTerminalResponseObserver =
-                object : StreamObserver<TerminalOuterClass.ListenTerminalResponse> {
-                    override fun onNext(response: TerminalOuterClass.ListenTerminalResponse) {
-                        when {
-                            response.hasTitle() -> {
-                                debug("Received terminal title: ${response.title}")
-                                val terminalContent =
-                                        terminalView.toolWindow.contentManager.getContent(
-                                                shellTerminalWidget
-                                        )
-                                terminalContent.putUserData(TITLE_KEY, response.title)
-                            }
-                            response.hasData() -> {
-                                debug("Printing a text on '${supervisorTerminal.title}' terminal.")
-                                dataReceivedFromSupervisor.write(response.data.toByteArray())
-                            }
-                            response.hasExitCode() -> {
-                                debug(
-                                        "Closing '${supervisorTerminal.title}' terminal (Exit Code: ${response.exitCode}."
-                                )
-                                shellTerminalWidget.close()
-                            }
-                        }
-                    }
-
-                    override fun onCompleted() {
-                        debug("'${supervisorTerminal.title}' terminal finished reading stream.")
-                    }
-
-                    override fun onError(throwable: Throwable) {
-                        if (containsTerminatedStatus(throwable)) return
-
-                        thisLogger()
-                                .error(
-                                        "Got an error while listening to '${supervisorTerminal.title}' terminal.",
-                                        throwable
-                                )
-                    }
-                }
-
-        terminalServiceStub.listen(listenTerminalRequest, listenTerminalResponseObserver)
-
-        /** Controls the writing flow, ensuring we send only one write request at a time. */
-        var isWaitingForWritingResponse = false
-
-        val writeTerminalResponseObserver =
-                object : StreamObserver<TerminalOuterClass.WriteTerminalResponse> {
-                    override fun onNext(response: TerminalOuterClass.WriteTerminalResponse) {
-                        debug(
-                                "${response.bytesWritten} bytes written on '${supervisorTerminal.title}' terminal."
-                        )
-                        isWaitingForWritingResponse = false
-                    }
-
-                    override fun onError(throwable: Throwable) {
-                        isWaitingForWritingResponse = false
-
-                        if (containsTerminatedStatus(throwable)) return
-
-                        thisLogger()
-                                .error(
-                                        "Got an error while writing to '${supervisorTerminal.title}' terminal.",
-                                        throwable
-                                )
-                    }
-
-                    override fun onCompleted() {
-                        debug("'${supervisorTerminal.title}' terminal finished writing stream.")
-                        isWaitingForWritingResponse = false
-                    }
-                }
-
-        val writeTerminalRequestBuilder =
-                TerminalOuterClass.WriteTerminalRequest.newBuilder()
-                        .setAlias(supervisorTerminal.alias)
-
-        val watchTerminalInputJob =
-                GlobalScope.launch {
-                    withContext(Dispatchers.IO) {
-                        while (isActive) {
-                            if (dataReceivedFromSupervisor.size() > 0) {
-                                val bytes = dataReceivedFromSupervisor.toByteArray()
-                                dataReceivedFromSupervisor.reset()
-                                terminalOutputWriter.write(bytes)
-                                terminalOutputWriter.flush()
-                            }
-
-                            val bytesAvailableOnTerminalInput = terminalInputReader.available()
-
-                            if (!isWaitingForWritingResponse && bytesAvailableOnTerminalInput > 0) {
-                                isWaitingForWritingResponse = true
-
-                                val bytesFromTerminalInput =
-                                        ByteArray(bytesAvailableOnTerminalInput)
-
-                                terminalInputReader.read(
-                                        bytesFromTerminalInput,
-                                        0,
-                                        bytesAvailableOnTerminalInput
-                                )
-
-                                val supervisorTerminalStdin =
-                                        ByteString.copyFrom(bytesFromTerminalInput)
-                                val writeTerminalRequest =
-                                        writeTerminalRequestBuilder
-                                                .setStdin(supervisorTerminalStdin)
-                                                .build()
-
-                                terminalServiceStub.write(
-                                        writeTerminalRequest,
-                                        writeTerminalResponseObserver
-                                )
-                            }
-                        }
-                    }
-                }
-
-        shellTerminalWidget.addListener {
-            debug("Terminal '${supervisorTerminal.title}' was closed via IDE.")
-            watchTerminalInputJob.cancel()
-            val shutdownTerminalRequest =
-                    TerminalOuterClass.ShutdownTerminalRequest.newBuilder()
-                            .setAlias(supervisorTerminal.alias)
-                            .build()
-            val shutdownTerminalResponseObserver =
-                    object : StreamObserver<TerminalOuterClass.ShutdownTerminalResponse> {
-                        override fun onNext(response: TerminalOuterClass.ShutdownTerminalResponse) =
-                                Unit
-
-                        override fun onError(throwable: Throwable) {
-                            if (containsTerminatedStatus(throwable)) return
-
-                            thisLogger()
-                                    .error(
-                                            "Got an error while trying to shutdown '${supervisorTerminal.title}' terminal.",
-                                            throwable
-                                    )
-                        }
-
-                        override fun onCompleted() {
-                            debug("Successfully shutdown '${supervisorTerminal.title}' terminal.")
-                        }
-                    }
-            terminalServiceStub.shutdown(shutdownTerminalRequest, shutdownTerminalResponseObserver)
-        }
-    }
-
-    private fun containsTerminatedStatus(throwable: Throwable) =
-            throwable is StatusRuntimeException &&
-                    throwable.status.description == "signal: terminated"
 
     private fun debug(message: String) = runInEdt {
         if (System.getenv("JB_DEV").toBoolean()) thisLogger().warn(message)
