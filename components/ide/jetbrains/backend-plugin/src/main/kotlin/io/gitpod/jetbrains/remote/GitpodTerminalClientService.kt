b7e0bafa425efa7b070954dc544a3b741db98a5b
Victor Nogueira <victor@gitpod.io>
2022-04-06 09:20:39 +0000

    Revert "Open terminals when guest client connects"
    This reverts commit 7e68a74b842a0ed20d674fe3c0f93e51f75d3a26.

--- components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodTerminalClientService.kt
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodTerminalClientService.kt
@@ -4,153 +4,23 @@
 
 package io.gitpod.jetbrains.remote
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.client.ClientProjectSession
-import com.intellij.openapi.diagnostic.thisLogger
-import com.intellij.openapi.util.Key
-import com.jetbrains.rd.util.lifetime.Lifetime
-import com.jetbrains.rdserver.terminal.BackendTerminalManager
-import io.gitpod.supervisor.api.Status
-import io.gitpod.supervisor.api.StatusServiceGrpc
-import io.gitpod.supervisor.api.TerminalOuterClass
-import io.gitpod.supervisor.api.TerminalServiceGrpc
-import io.grpc.stub.StreamObserver
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.future.await
-import kotlinx.coroutines.guava.asDeferred
-import kotlinx.coroutines.launch
 import org.jetbrains.plugins.terminal.TerminalView
-import java.util.concurrent.CompletableFuture
 
-@Suppress("EXPERIMENTAL_IS_NOT_ENABLED", "UnstableApiUsage", "OPT_IN_IS_NOT_ENABLED")
-@OptIn(DelicateCoroutinesApi::class)
-class GitpodTerminalClientService(session: ClientProjectSession) : Disposable {
-    companion object {
-        val TITLE_KEY = Key.create<String>("TITLE_KEY")
-    }
-
-    private val project = session.project
-    private val lifetime = Lifetime.Eternal.createNested()
-    private val terminalView = TerminalView.getInstance(project)
-    private val terminalServiceFutureStub =
-            TerminalServiceGrpc.newFutureStub(GitpodManager.supervisorChannel)
-    private val statusServiceStub = StatusServiceGrpc.newStub(GitpodManager.supervisorChannel)
-    private val backendTerminalManager = BackendTerminalManager.getInstance(project)
-
-    override fun dispose() {
-        lifetime.terminate()
-    }
-
+@Suppress("UnstableApiUsage")
+class GitpodTerminalClientService(session: ClientProjectSession) {
     init {
-        GlobalScope.launch {
-            val tasksList = getSupervisorTasksList().await()
-            val terminalsList = getSupervisorTerminalsListAsync().await().terminalsList
-            debug("Got a list of Supervisor terminals: $terminalsList")
-            runInEdt {
-//                for (terminalWidget in terminalView.widgets) {
-//                    val terminalContent =
-//                            terminalView.toolWindow.contentManager.getContent(terminalWidget)
-//                    val terminalTitle = terminalContent.getUserData(TITLE_KEY)
-//                    if (terminalTitle != null) {
-//                        debug("Closing terminal $terminalTitle before opening it again.")
-//                        terminalWidget.close()
-//                    }
-//                }
-
-                if (tasksList.isEmpty() || terminalsList.isEmpty()) {
-                    backendTerminalManager.createNewSharedTerminal("GitpodTerminal", "Terminal")
-                } else {
-                    val aliasToTerminalMap: MutableMap<String, TerminalOuterClass.Terminal> =
-                            mutableMapOf()
-
-                    for (terminal in terminalsList) {
-                        val terminalAlias = terminal.alias
-                        aliasToTerminalMap[terminalAlias] = terminal
-                    }
-
-                    for (task in tasksList) {
-                        val terminalAlias = task.terminal
-                        val terminal = aliasToTerminalMap[terminalAlias]
-
-                        if (terminal != null) {
-                            createSharedTerminal(terminal)
-                        }
-                    }
-                }
+        runInEdt {
+            val project = session.project
+            val terminalView = TerminalView.getInstance(project)
+            for (widget in terminalView.widgets) {
+                val widgetContent = terminalView.toolWindow.contentManager.getContent(widget)
+                val terminalRunner = TerminalView.getRunnerByContent(widgetContent)
+                // The following deprecated method needs to be used, otherwise not all terminals appear
+                // when the Thin Client connects.
+                @Suppress("DEPRECATION") terminalRunner?.openSessionInDirectory(widget, "")
             }
         }
-    }
-
-    private fun getSupervisorTasksList(): CompletableFuture<List<Status.TaskStatus>> {
-        val completableFuture = CompletableFuture<List<Status.TaskStatus>>()
-        val taskStatusRequest = Status.TasksStatusRequest.newBuilder().setObserve(true).build()
-        val taskStatusResponseObserver =
-                object : StreamObserver<Status.TasksStatusResponse> {
-                    override fun onNext(response: Status.TasksStatusResponse) {
-                        debug("Received task list: ${response.tasksList}")
-
-                        var hasOpenedAllTasks = true
-
-                        response.tasksList.forEach { task ->
-                            if (task.state === Status.TaskState.opening) {
-                                hasOpenedAllTasks = false
-                            }
-                        }
-
-                        if (hasOpenedAllTasks) {
-                            this.onCompleted()
-                            completableFuture.complete(response.tasksList)
-                        }
-                    }
-
-                    override fun onCompleted() {
-                        debug("Successfully fetched tasks from Supervisor.")
-                    }
-
-                    override fun onError(throwable: Throwable) {
-                        thisLogger()
-                                .error(
-                                        "Got an error while trying to fetch tasks from Supervisor.",
-                                        throwable
-                                )
-                        completableFuture.completeExceptionally(throwable)
-                    }
-                }
-
-        statusServiceStub.tasksStatus(taskStatusRequest, taskStatusResponseObserver)
-
-        return completableFuture
-    }
-
-    private fun getSupervisorTerminalsListAsync(): Deferred<TerminalOuterClass.ListTerminalsResponse> {
-        val listTerminalsRequest = TerminalOuterClass.ListTerminalsRequest.newBuilder().build()
-        return terminalServiceFutureStub.list(listTerminalsRequest).asDeferred()
-    }
-
-    private fun createSharedTerminal(supervisorTerminal: TerminalOuterClass.Terminal) {
-        debug("Creating shared terminal '${supervisorTerminal.title}' on Backend IDE")
-
-        val shellTerminalWidget = terminalView.createLocalShellWidget(project.basePath, supervisorTerminal.title, false)
-
-        val terminalContent = terminalView.toolWindow.contentManager.getContent(shellTerminalWidget)
-
-        terminalContent.putUserData(TITLE_KEY, supervisorTerminal.title)
-
-        val terminalRunner = TerminalView.getRunnerByContent(terminalContent)
-
-        backendTerminalManager.shareTerminal(shellTerminalWidget, terminalRunner.toString())
-
-        // The following deprecated method needs to be used, otherwise not all terminals appear
-        // when the Thin Client connects.
-        // @Suppress("DEPRECATION") terminalRunner?.openSessionInDirectory(shellTerminalWidget, "")
-
-        shellTerminalWidget.executeCommand("gp tasks attach ${supervisorTerminal.alias}")
-    }
-
-    private fun debug(message: String) = runInEdt {
-        if (System.getenv("JB_DEV").toBoolean()) thisLogger().warn(message)
     }
 }
---
+++ components/ide/jetbrains/backend-plugin/src/main/kotlin/io/gitpod/jetbrains/remote/GitpodTerminalService.kt
@@ -0,0 +1,176 @@
+// Copyright (c) 2022 Gitpod GmbH. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package io.gitpod.jetbrains.remote
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener
+import com.jetbrains.rd.util.lifetime.Lifetime
+import com.jetbrains.rdserver.terminal.BackendTerminalManager
+import io.gitpod.supervisor.api.Status.*
+import io.gitpod.supervisor.api.StatusServiceGrpc
+import io.gitpod.supervisor.api.TerminalOuterClass
+import io.gitpod.supervisor.api.TerminalServiceGrpc
+import io.grpc.stub.StreamObserver
+import java.util.concurrent.CompletableFuture
+import kotlinx.coroutines.*
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.guava.asDeferred
+import org.jetbrains.plugins.terminal.TerminalToolWindowFactory
+import org.jetbrains.plugins.terminal.TerminalView
+
+@Suppress("UnstableApiUsage", "EXPERIMENTAL_IS_NOT_ENABLED")
+@OptIn(DelicateCoroutinesApi::class)
+class GitpodTerminalService(private val project: Project) : Disposable {
+    companion object {
+        val TITLE_KEY = Key.create<String>("TITLE_KEY")
+    }
+
+    private val lifetime = Lifetime.Eternal.createNested()
+    private val terminalView = TerminalView.getInstance(project)
+    private val terminalServiceFutureStub =
+            TerminalServiceGrpc.newFutureStub(GitpodManager.supervisorChannel)
+    private val statusServiceStub = StatusServiceGrpc.newStub(GitpodManager.supervisorChannel)
+    private val backendTerminalManager = BackendTerminalManager.getInstance(project)
+
+    override fun dispose() {
+        lifetime.terminate()
+    }
+
+    init {
+        GlobalScope.launch {
+            getTerminalToolWindowRegisteredEvent().await()
+            val tasksList = getSupervisorTasksList().await()
+            val terminalsList = getSupervisorTerminalsListAsync().await().terminalsList
+            debug("Got a list of Supervisor terminals: $terminalsList")
+            runInEdt {
+                for (terminalWidget in terminalView.widgets) {
+                    val terminalContent =
+                            terminalView.toolWindow.contentManager.getContent(terminalWidget)
+                    val terminalTitle = terminalContent.getUserData(TITLE_KEY)
+                    if (terminalTitle != null) {
+                        debug("Closing terminal $terminalTitle before opening it again.")
+                        terminalWidget.close()
+                    }
+                }
+
+                if (tasksList.isEmpty() || terminalsList.isEmpty()) {
+                    backendTerminalManager.createNewSharedTerminal("GitpodTerminal", "Terminal")
+                } else {
+                    val aliasToTerminalMap: MutableMap<String, TerminalOuterClass.Terminal> =
+                            mutableMapOf()
+
+                    for (terminal in terminalsList) {
+                        val terminalAlias = terminal.alias
+                        aliasToTerminalMap[terminalAlias] = terminal
+                    }
+
+                    for (task in tasksList) {
+                        val terminalAlias = task.terminal
+                        val terminal = aliasToTerminalMap[terminalAlias]
+
+                        if (terminal != null) {
+                            createSharedTerminal(terminal)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getTerminalToolWindowRegisteredEvent(): CompletableFuture<Void> {
+        val completableFuture = CompletableFuture<Void>()
+
+        debug("Waiting for TerminalToolWindow to be registered...")
+        val toolWindowManagerListener =
+                object : ToolWindowManagerListener {
+                    override fun toolWindowsRegistered(
+                            ids: MutableList<String>,
+                            toolWindowManager: ToolWindowManager
+                    ) {
+                        if (ids.contains(TerminalToolWindowFactory.TOOL_WINDOW_ID)) {
+                            debug("TerminalToolWindow got registered!")
+                            completableFuture.complete(null)
+                        }
+                    }
+                }
+
+        project.messageBus
+                .connect()
+                .subscribe(ToolWindowManagerListener.TOPIC, toolWindowManagerListener)
+
+        return completableFuture
+    }
+
+    private fun getSupervisorTasksList(): CompletableFuture<List<TaskStatus>> {
+        val completableFuture = CompletableFuture<List<TaskStatus>>()
+        val taskStatusRequest = TasksStatusRequest.newBuilder().setObserve(true).build()
+        val taskStatusResponseObserver =
+                object : StreamObserver<TasksStatusResponse> {
+                    override fun onNext(response: TasksStatusResponse) {
+                        debug("Received task list: ${response.tasksList}")
+
+                        var hasOpenedAllTasks = true
+
+                        response.tasksList.forEach { task ->
+                            if (task.state === TaskState.opening) {
+                                hasOpenedAllTasks = false
+                            }
+                        }
+
+                        if (hasOpenedAllTasks) {
+                            this.onCompleted()
+                            completableFuture.complete(response.tasksList)
+                        }
+                    }
+
+                    override fun onCompleted() {
+                        debug("Successfully fetched tasks from Supervisor.")
+                    }
+
+                    override fun onError(throwable: Throwable) {
+                        thisLogger()
+                                .error(
+                                        "Got an error while trying to fetch tasks from Supervisor.",
+                                        throwable
+                                )
+                        completableFuture.completeExceptionally(throwable)
+                    }
+                }
+
+        statusServiceStub.tasksStatus(taskStatusRequest, taskStatusResponseObserver)
+
+        return completableFuture
+    }
+
+    private fun getSupervisorTerminalsListAsync(): Deferred<TerminalOuterClass.ListTerminalsResponse> {
+        val listTerminalsRequest = TerminalOuterClass.ListTerminalsRequest.newBuilder().build()
+        return terminalServiceFutureStub.list(listTerminalsRequest).asDeferred()
+    }
+
+    private fun createSharedTerminal(supervisorTerminal: TerminalOuterClass.Terminal) {
+        debug("Creating shared terminal '${supervisorTerminal.title}' on Backend IDE")
+
+        val terminalRunnerId = supervisorTerminal.alias
+
+        val shellTerminalWidget = terminalView.createLocalShellWidget(project.basePath, terminalRunnerId)
+
+        backendTerminalManager.shareTerminal(shellTerminalWidget, terminalRunnerId)
+
+        val terminalContent = terminalView.toolWindow.contentManager.getContent(shellTerminalWidget)
+
+        terminalContent.putUserData(TITLE_KEY, supervisorTerminal.title)
+
+        shellTerminalWidget.executeCommand("gp tasks attach ${supervisorTerminal.alias}")
+    }
+
+    private fun debug(message: String) = runInEdt {
+        if (System.getenv("JB_DEV").toBoolean()) thisLogger().warn(message)
+    }
+}
